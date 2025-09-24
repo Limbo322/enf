2a181dcddc678e714f3ed3eebeb3481b0613c97b
Limbo322 <ayrandzhyan@yandex.ru>
2025-09-24 14:01:59 +0300

    сделали views/urls/admin

--- enf/urls.py
+++ enf/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls', namespace='main')),
+    
 ]
 
 if settings.DEBUG:
